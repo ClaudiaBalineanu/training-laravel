22a5f83f17c7c3f07115237b9f140d177238afab
sdbisTeam <balineanu@gmail.com>
2020-02-13 18:50:39 +0200

    for login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -39,9 +40,15 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    /**
+     * The logout function
+     *
+     * @return RedirectResponse
+     */
     public function logout()
     {
         Auth::logout();
+
         return redirect()->route('login');
     }
 }
