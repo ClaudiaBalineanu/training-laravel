a12d28ea6548c35208fff7d99befd289ec0b1629
Claudia Balineanu <claudia.balineanu@5psolutions.eu>
2020-02-28 10:26:19 +0200

    orders spa comm

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -9,11 +9,15 @@ class OrdersController extends Controller
     /**
      * Display all orders.
      *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
      */
     public function index()
     {
         $orders = Order::with('value')->get();
+
+        if (request()->ajax()) {
+            return $orders;
+        }
 
         return view('orders.index', compact('orders'));
     }
@@ -22,10 +26,16 @@ class OrdersController extends Controller
      * Display an order with specific products.
      *
      * @param Order $order
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return Order
      */
     public function show(Order $order)
     {
+        $order['value'] = $order->value()->first();
+
+        if (request()->ajax()) {
+            return response()->json(['order' => $order, 'products' => $order->products]);
+        }
+
         return view('orders.order', compact('order'));
     }
 
